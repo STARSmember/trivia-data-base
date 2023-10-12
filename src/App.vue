d292fc918aa890614126bb1dcb50396f87cdf9c2
Julian Fragoso <starsmember@yahoo.com>
2023-10-11 23:02:08 -0500

    added sublime colors

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
+  <main>
   <RouterView />
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
+}
+</style>
