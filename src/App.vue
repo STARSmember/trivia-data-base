1e0ff5f816cab1b9da9472b2e1d341227c1a13fe
Julian Fragoso <starsmember@yahoo.com>
2023-10-11 23:16:08 -0500

    changed syntax

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 
 <template>
   <main>
-  <RouterView />
+    <div class="card">
+      <RouterView />
+    </div>
   </main>
 </template>
 
 <style lang="postcss" scoped>
 main {
-  @apply h-screen bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
+  @apply  flex h-screen justify-center items-center bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
 }
 </style>
