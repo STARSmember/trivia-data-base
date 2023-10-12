cc89ecf2aa36f3371c08b80345b1272b280471d4
Julian Fragoso <starsmember@yahoo.com>
2023-10-11 23:24:10 -0500

    tired renaming

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <style lang="postcss" scoped>
 main {
-  @apply  flex h-screen justify-center items-center bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
+  @apply .flex .h-screen justify-center .items-center bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
   & .card {
     @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
   }
