029227e9ad766f5c34645f65b37d028b1772d5c4
Julian Fragoso <starsmember@yahoo.com>
2023-10-11 22:50:21 -0500

    center question page title

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,5 +1,5 @@
+<script setup>
+</script>
 <template>
-  <main class="flex min-h-screen items-center justify-center">
     <h1 class="text-6xl font-thin text-slate-800">I'm Question Page</h1>
-    </main>
 </template>
