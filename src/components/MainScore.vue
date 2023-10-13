c8b9f3ec6cc8bc8ece705f2aa9f9c3fde85e7a6e
Julian Fragoso <starsmember@yahoo.com>
2023-10-13 11:54:07 -0500

    adding script setup to fix syntax

---
+++ src/components/MainScore.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+    <p class="score">Score: 100</p>
+</template>
+
+<style lang="postcss" scoped>
+.score {
+  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-700;
+}
+</style>
