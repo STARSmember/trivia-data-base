9645f0b436b8064fd924df7196280f91144ed5a2
Julian Fragoso <starsmember@yahoo.com>
2023-10-12 20:01:20 -0500

    Deleting 00

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -31,7 +31,7 @@
     @apply grid flex-grow grid-cols-4 gap-12;
 
     & .category {
-      @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500000 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+      @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
 
       &:hover {
         @apply cursor-pointer border-red-500 bg-red-500 text white;
