d29e64b0cd86047da914bfd1d9cf7faa82d3930d
Julian Fragoso <starsmember@yahoo.com>
2023-10-14 12:49:30 -0500

    deleted prettier

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,8 +1,13 @@
 <script setup>
 import BaseTitle from "@/components/BaseTitle.vue"
+import { useRoute } from "vue-router"
+
+const route = useRoute();
+
+const categoryID = route.params.id; 
 </script>
 
 <template>
-    <BaseTitle> I'm Question Page </BaseTitle>
+    <BaseTitle> I'm Question Page for {{ categoryID }}</BaseTitle>
 </template>
  
